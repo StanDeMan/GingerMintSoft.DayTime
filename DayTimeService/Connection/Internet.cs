ef96cbf4a841fc030311ccad86ae051acd2038ae
StanDeMan <StanislausE@gmx.de>
2024-02-18 13:44:10 +0100

    Check if internet accessible via google service

---
+++ DayTimeService/Connection/Internet.cs
@@ -0,0 +1,29 @@
+﻿namespace DayTimeService.Connection
+{
+    public static class Internet
+    {
+        
+        /// <summary>
+        /// Simple internet check
+        /// </summary>
+        /// <returns><c>true</c> on success.</returns>
+        // ReSharper disable once UnusedMember.Global
+        public static async Task<bool> CheckAccessibilityAsync()
+        {
+            const string google = "http://google.com/generate_204";
+
+            try
+            {
+                using var client = new HttpClient();
+                using (await client.GetAsync(google))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
