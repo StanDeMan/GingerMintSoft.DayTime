59e7e352bce8fc9ee25b2fb98dbbe121be04adb2
StanDeMan <StanislausE@gmx.de>
2024-02-19 14:01:22 +0100

    First DayTime draft - only for first implementation

---
+++ DayTimeService/Day/DayTime.cs
@@ -0,0 +1,23 @@
+﻿using GingerMintSoft.DayTime;
+
+namespace DayTimeService.Day
+{
+    public class DayTime
+    {
+        public void ReadSunTimes()
+        {
+            var home = new Coordinates()
+            {
+                Latitude = 48.10507778308992,
+                Longitude = 7.90856839921184
+            };
+
+            // iterate thru whole year 
+            var actDate = DateTime.Now;
+
+            // Parameters : year - month - day - lat - long
+            // ReSharper disable once UnusedVariable
+            var actDay = new CalcDayTime().SunriseSunset(actDate, home);
+        }
+     }
+}
