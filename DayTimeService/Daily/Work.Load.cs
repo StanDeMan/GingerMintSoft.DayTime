beb75edd70b403fc433a903f94746a4f871197f9
StanDeMan <StanislausE@gmx.de>
2024-02-20 21:30:04 +0100

    First draft for the workload to read in the json

---
+++ DayTimeService/Daily/Work.Load.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace DayTimeService.Daily
+{
+    public class Application
+    {
+        public Workload? ReadWorkload(string path)
+        {
+            using StreamReader reader = new(path);
+            var json = reader.ReadToEnd();
+            var workLoad = JsonConvert.DeserializeObject<Workload>(json);
+        
+            return workLoad;
+        }
+    }
+
+    public class Workload
+    {
+        public required Program Program { get; set; }
+    }
+
+    public class Program
+    {
+        public required string TaskId { get; set; }
+        public required Coordinate Coordinate { get; set; }
+        public required List<Task> Tasks { get; set; }
+    }
+
+    public class Coordinate
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class Task
+    {
+        public required string TaskId { get; set; }
+        public required string Command { get; set; }
+    }
+}
