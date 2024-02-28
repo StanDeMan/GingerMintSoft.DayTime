a63af9fec9e6cb9f7415e1c0d9324fdfc4a4e48d
StanDeMan <StanislausE@gmx.de>
2024-02-29 00:54:12 +0100

    Persist preliminary

--- DayTimeService/DayTimeServiceWorker.cs
+++ DayTimeService/DayTimeServiceWorker.cs
@@ -3,21 +3,18 @@ using DayTimeService.Daily;
 using DayTimeService.Execute;
 using DayTimeService.Hardware;
 using GingerMintSoft.DayTime.Scheduler;
+using Quartz;
+using Quartz.Impl;
 using Task = System.Threading.Tasks.Task;
 using DayTimeTask = GingerMintSoft.DayTime.Scheduler.Task;
 
 namespace DayTimeService
 {
-    public class DayTimeServiceWorker : BackgroundService
+    public class DayTimeServiceWorker(ILogger<DayTimeServiceWorker> logger) : BackgroundService
     {
         public enum Day { SunRise = 0, SunSet = 1, Undefined = 9999 }
         private bool _ledOn;
-        private readonly ILogger<DayTimeServiceWorker> _logger;
-
-        public DayTimeServiceWorker(ILogger<DayTimeServiceWorker> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ILogger<DayTimeServiceWorker> _logger = logger;
 
         /// <summary>
         /// Long term service
@@ -43,6 +40,25 @@ namespace DayTimeService
                 "DayTimeServiceWorker is executed at: {time} with recurrence {time}", 
                 startingTime, 
                 recurrence);
+
+            var scheduler = await new StdSchedulerFactory().GetScheduler(stoppingToken);
+            await scheduler.Start(stoppingToken);
+ 
+            var job = JobBuilder.Create<DayTimeJob>()
+                .WithIdentity(execute.Program.TaskId)
+                //.UsingJobData("someKey", execute)
+                .Build();
+ 
+            var trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule(s => 
+                    s.WithIntervalInHours(Convert.ToInt32(recurrence.TotalHours))
+                        .OnEveryDay()
+                        .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(
+                            startingTime.Hour, 
+                            startingTime.Minute))
+                    ).Build();
+
+            await scheduler.ScheduleJob(job, trigger, stoppingToken);
 
             var recurringTask = new RecurringTask(() =>
                 {
@@ -86,6 +102,18 @@ namespace DayTimeService
 
                 await Task.Delay(250, stoppingToken);
             }
+        }
+    }
+
+    public class DayTimeJob : IJob
+    {
+        public Workload? Workload { get; set; }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            var dataMap = context.JobDetail.JobDataMap;
+            //TypeT someValue = <TypeT>dataMap.Get("someKey");
+            return Task.CompletedTask;
         }
     }
 }
