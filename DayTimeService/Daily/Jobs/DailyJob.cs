572f6e166585a06cc950f3f7c7f6e2daa6f4e224
Stanislaus Eichstaedt <stanislaus.eichstaedt@ch.sauter-bc.com>
2024-02-29 17:29:27 +0100

    Implement first job draft

---
+++ DayTimeService/Daily/Jobs/DailyJob.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Quartz;
+using JobTask = System.Threading.Tasks.Task;
+
+namespace DayTimeService.Daily.Jobs
+{
+    public class DailyJob : IJob
+    {
+        public JobTask Execute(IJobExecutionContext context)
+        {
+            var dataMap = context.JobDetail.JobDataMap;
+            var execute = JsonConvert.DeserializeObject<Workload>(dataMap.GetString("Execute")!);
+
+            //actual date and time set to midnight
+            var now = DateTime.Now.ToLocalTime();
+            var actDate = new DateTime(now.Year, now.Month, now.Day);
+
+            var day = Define.CalcSunRiseSunSet(actDate, execute!);
+            
+            //var firstTask = execute!.Program.Tasks[0];
+
+
+            return JobTask.CompletedTask;
+        }
+    }
+}
--- DayTimeService/DayTimeServiceWorker.cs
+++ DayTimeService/DayTimeServiceWorker.cs
@@ -1,12 +1,12 @@
 using System.Reflection;
 using DayTimeService.Daily;
+using DayTimeService.Daily.Jobs;
 using DayTimeService.Execute;
 using DayTimeService.Hardware;
-using GingerMintSoft.DayTime.Scheduler;
+using Newtonsoft.Json;
 using Quartz;
 using Quartz.Impl;
 using Task = System.Threading.Tasks.Task;
-using DayTimeTask = GingerMintSoft.DayTime.Scheduler.Task;
 
 namespace DayTimeService
 {
@@ -43,10 +43,10 @@ namespace DayTimeService
 
             var scheduler = await new StdSchedulerFactory().GetScheduler(stoppingToken);
             await scheduler.Start(stoppingToken);
- 
-            var job = JobBuilder.Create<DayTimeJob>()
+
+            var job = JobBuilder.Create<DailyJob>()
                 .WithIdentity(execute.Program.TaskId)
-                //.UsingJobData("someKey", execute)
+                .UsingJobData("Execute", JsonConvert.SerializeObject(execute))
                 .Build();
  
             var trigger = TriggerBuilder.Create()
@@ -54,45 +54,13 @@ namespace DayTimeService
                     s.WithIntervalInHours(Convert.ToInt32(recurrence.TotalHours))
                         .OnEveryDay()
                         .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(
-                            startingTime.Hour, 
-                            startingTime.Minute))
+                            startingTime.Hour,
+                            startingTime.Minute)
+                        //.StartingDailyAt(new TimeOfDay(DateTime.Now.Hour, DateTime.Now.Minute,DateTime.Now.Second)
+                        )
                     ).Build();
 
             await scheduler.ScheduleJob(job, trigger, stoppingToken);
-
-            var recurringTask = new RecurringTask(() =>
-                {
-                    //actual date and time set to midnight
-                    var now = DateTime.Now.ToLocalTime();
-                    var actDate = new DateTime(now.Year, now.Month, now.Day);
-
-                    var day = Define.CalcSunRiseSunSet(actDate, execute);
-
-                    foreach (var taskToExec in execute.Program.Tasks.OrderBy(tsk => tsk.Id))
-                    {
-                        //AddTask(new DayTimeTask()
-                        //{
-                        //    TaskId = taskToExec.TaskId,
-                        //    StartTime = taskToExec.Id == Convert.ToInt32(Day.SunRise)
-                        //        ? day.SunRise
-                        //        : day.SunSet,
-                        //    TaskAction = () =>
-                        //    {
-                        //        var bOk = Command.Execute(taskToExec.Command);
-
-                        //        _logger.LogInformation(
-                        //            "DayTimeServiceWorker executing Task {string} with command: {string} at {time}. Executed: {bool}",
-                        //            taskToExec.Command,
-                        //            taskToExec.TaskId,
-                        //            DateTimeOffset.Now.ToLocalTime(),
-                        //            bOk);
-                        //    }
-                        //});
-                    }
-                },
-                startingTime,
-                recurrence,
-                execute.Program.TaskId);
 
             while (!stoppingToken.IsCancellationRequested)
             {
@@ -105,15 +73,37 @@ namespace DayTimeService
         }
     }
 
-    public class DayTimeJob : IJob
-    {
-        public Workload? Workload { get; set; }
+    //var recurringTask = new RecurringTask(() =>
+    //    {
+    //        //actual date and time set to midnight
+    //        var now = DateTime.Now.ToLocalTime();
+    //        var actDate = new DateTime(now.Year, now.Month, now.Day);
 
-        public Task Execute(IJobExecutionContext context)
-        {
-            var dataMap = context.JobDetail.JobDataMap;
-            //TypeT someValue = <TypeT>dataMap.Get("someKey");
-            return Task.CompletedTask;
-        }
-    }
+    //        var day = Define.CalcSunRiseSunSet(actDate, execute);
+
+    //        foreach (var taskToExec in execute.Program.Tasks.OrderBy(tsk => tsk.Id))
+    //        {
+    //            AddTask(new DayTimeTask()
+    //            {
+    //                TaskId = taskToExec.TaskId,
+    //                StartTime = taskToExec.Id == Convert.ToInt32(Day.SunRise)
+    //                    ? day.SunRise
+    //                    : day.SunSet,
+    //                TaskAction = () =>
+    //                {
+    //                    var bOk = Command.Execute(taskToExec.Command);
+
+    //                    _logger.LogInformation(
+    //                        "DayTimeServiceWorker executing Task {string} with command: {string} at {time}. Executed: {bool}",
+    //                        taskToExec.Command,
+    //                        taskToExec.TaskId,
+    //                        DateTimeOffset.Now.ToLocalTime(),
+    //                        bOk);
+    //                }
+    //            });
+    //        }
+    //    },
+    //    startingTime,
+    //    recurrence,
+    //    execute.Program.TaskId);
 }
