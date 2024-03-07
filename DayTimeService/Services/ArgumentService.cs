fd6e48d9983b9de9ab3b38cf22a475d16ecbd9fe
Stanislaus Eichstaedt <stanislaus.eichstaedt@ch.sauter-bc.com>
2024-03-07 20:24:10 +0100

    Add argument service again to hold cli data

---
+++ DayTimeService/Services/ArgumentService.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using CommandLine;
+using DayTimeService.CmdLine;
+
+namespace DayTimeService.Services
+{
+    public class ArgumentService(ParserResult<Options> args)
+    {
+        private readonly Arguments _arguments = new();
+
+        public Arguments Read()
+        {
+            try
+            {
+                _arguments.WorkloadFile = args.Value.Filename;
+            }
+            catch (Exception)
+            {
+                _arguments.Errors = args.Errors;
+            }
+
+            return _arguments;
+        }
+    }
+
+    public class Arguments
+    {
+        public string WorkloadFileDefaultName { get; private set; } = new StringBuilder().Append("DailyWorkload.json").ToString();
+        public string? WorkloadFile { get; set; }
+        public IEnumerable<Error>? Errors { get; set; }
+    }
+}
