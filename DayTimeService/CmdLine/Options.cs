739b9fc6adab0ba2672669621e00d6d45af2cb53
Stanislaus Eichstaedt <stanislaus.eichstaedt@ch.sauter-bc.com>
2024-03-07 20:23:01 +0100

    Options for cli

---
+++ DayTimeService/CmdLine/Options.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace DayTimeService.CmdLine
+{
+    public class Options
+    {
+        [Option("filename", Required = false, HelpText = "Input filename.")]
+        public string? Filename { get; set; }
+
+        [Usage(ApplicationAlias = "DayTime")]
+        public static IEnumerable<Example> Examples => 
+        [
+            new Example("Calculates sun rise and sun set times using the data from this file.", new Options { Filename = "DailyWorkload.json" })
+        ];
+    }
+}
