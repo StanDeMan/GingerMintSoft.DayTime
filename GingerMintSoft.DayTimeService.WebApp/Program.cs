1290322ba7ca92b5495d60bbfa9bb7dccbf9c201
StanDeMan <StanislausE@gmx.de>
2024-03-17 22:43:38 +0100

    Add systemd to webapp

--- GingerMintSoft.DayTimeService.WebApp/Program.cs
+++ GingerMintSoft.DayTimeService.WebApp/Program.cs
@@ -1,8 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddSystemd();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,9 +18,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
